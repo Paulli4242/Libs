ed5f5dcdec52a2dbe68f24d95c8c269acadfe31d
Paulli <61700276+Paulli4242@users.noreply.github.com>
2021-06-15 08:08:56 +0200

    remove net

--- src/xyz/dc_stats/net/Packet.java
+++
@@ -1,6 +0,0 @@
-package xyz.dc_stats.net;
-
-public interface Packet {
-    byte[] serialize();
-    void deserialize(byte[] bts);
-}
--- src/xyz/dc_stats/net/PacketFormat.java
+++
@@ -1,7 +0,0 @@
-package xyz.dc_stats.net;
-
-public interface PacketFormat{
-
-    byte[] formatSend(Packet packet);
-    Packet formatReceive(byte[] bts) throws PacketFormatException;
-}

a721171fa28db843f20b4d694046900f104a1fd2
Paulli <61700276+Paulli4242@users.noreply.github.com>
2021-06-25 04:17:26 +0200

    updated ArgumentParser
    renamed ArgumentMap -> ArgumentParser
    update default system

--- src/xyz/dc_stats/args/ArgumentParser.java
+++ src/xyz/dc_stats/args/ArgumentParser.java
@@ -2,10 +2,10 @@ package xyz.dc_stats.args;
 
 import java.util.Arrays;
 
-public class ArgumentMap {
+public class ArgumentParser {
 	
 	private Node[] nodes = new Node[0];
-	public ArgumentMap(String[] args) {
+	public ArgumentParser(String[] args) {
 		String current = "";
 		Node currentV;
 		for(String s : args) {
@@ -26,28 +26,6 @@ public class ArgumentMap {
 			}
 		}
 	}
-	public ArgumentMap(String[] args, String[] defaults) {
-		this(args);
-		String current = "";
-		Node currentV;
-		for(String s : args) {
-			if(s.startsWith("--")) {
-				current = s.substring(2).toLowerCase();
-				if(!isKey(current)) {
-					nodes = Arrays.copyOfRange(nodes, 0, nodes.length+1);
-					nodes[nodes.length-1]=new Node(current);
-				}
-			}else if(!current.isEmpty()) {
-				if(getNode(current).getDefault()!=null) {
-					currentV = getNode(current);
-					currentV.setDefault(Arrays.copyOfRange(currentV.getDefault(), 0, currentV.getDefault().length+1));
-					currentV.getDefault()[currentV.getDefault().length-1]=s;
-				}else {
-					getNode(current).setDefault(new String[] {s});
-				}
-			}
-		}
-	}
 	public boolean isKey(String key) {
 		return getNode(key)!=null;
 	}
@@ -57,28 +35,48 @@ public class ArgumentMap {
 	}
 	public String getString(String key){
 		String[] s = getStringArray(key);
-		return s==null?null:getStringArray(key)[0];
+		return s==null?null:s[0];
 		
+	}
+
+	public String getString(String key, String def){
+		String s = getString(key);
+		return s==null?def:s;
 	}
 	public String[] getStringArray(String key) {
 			Node node = getNode(key.toLowerCase());
-			if(node!=null && node.getValueOrDefault() !=null)return node.getValueOrDefault();
+			if(node!=null)return node.getValue();
 			return null;
+	}
+	public String[] getStringArray(String key, String...def) {
+		String[] s = getStringArray(key);
+		return s==null?def:s;
 	}
 	public double getDouble(String key) {
 		return Double.parseDouble(getString(key));
 	}
+	public double getDouble(String key, double def) {
+		String s = getString(key);
+		return s==null?def:Double.parseDouble(s);
+	}
 	public int getInt(String key) {
 		return Integer.parseInt(getString(key));
 	}
+	public int getInt(String key,int def) {
+		String s = getString(key);
+		return s==null?def:Integer.parseInt(s);
+	}
 	public long getLong(String key) {
 			return Long.parseLong(getString(key));
+	}
+	public long getLong(String key,long def) {
+		String s = getString(key);
+		return s==null?def:Long.parseLong(s);
 	}
 	public class Node{
 		
 		String key;
 		String[] value;
-		String[] defaults;
 		
 		public Node(String key) {
 			this.key = key;
@@ -89,17 +87,8 @@ public class ArgumentMap {
 		public String[] getValue() {
 			return value;
 		}
-		private void setDefault(String[] value) {
-			this.defaults = value;
-		}
-		public String[] getDefault() {
-			return defaults;
-		}
 		public String getKey() {
 			return key;
-		}
-		public String[] getValueOrDefault(){
-			return value==null&&defaults!=null?defaults:value;
 		}
 	}
 }
